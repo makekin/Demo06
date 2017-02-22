d29e15d4e35639dd537d80d8e00cf70ed1b04347
k2751 <k2751@student.jamk.fi>
2017-02-22 16:27:18 +0200

    Teht03

---
+++ Teht02/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Teht02
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+  
+        private void EuroButtonClick(object sender, RoutedEventArgs e)
+        {
+            double number = double.Parse(input.Text);
+            number = number / 5.94573;
+            string value = number.ToString("0.00");
+            Result.Text = value;
+        }
+
+        private void MarkkaButtonClick(object sender, RoutedEventArgs e)
+        {
+            double number = double.Parse(input.Text);
+            number = number * 5.94573;
+            string value = number.ToString("0.00");
+            Result.Text = value;
+
+        }
+       
+    }
+}
---
+++ Teht03/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Teht03
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void textBlock6_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void textBlock7_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // lasin pinta ala lasketaan näin = lasin pinta-ala = (ikkunan leveys - karmin leveys *2) * (ikkunan pituus - karmin leveys * 2)
+    }
+}
